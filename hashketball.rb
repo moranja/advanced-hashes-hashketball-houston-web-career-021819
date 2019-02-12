646094284cdc49c978f3ed619d630c60751ae95f
Adam Moran <jeffadammoran@gmail.com>
2019-02-12 05:47:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,166 @@
-# Write your code here!
+require 'pry'
 
+def game_hash ()
+  hashketball = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        :Alan_Anderson => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1,
+        },
+        :Reggie_Evans => {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7,
+        },
+        :Brook_Lopez => {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 5,
+        },
+        :Mason_Plumlee => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5,
+        },
+        :Jason_Terry => {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1,
+        }
+      },
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        :Jeff_Adrien => {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2,
+        },
+        :Bismak_Biyombo => {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10,
+        },
+        :Desagna_Diop => {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5,
+        },
+        :Ben_Gordon => {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0,
+        },
+        :Brendan_Haywood => {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12,
+        }
+      }
+    }
+  }
+end
 
+def num_points_scored (player_name)
+  player_name_with_underscores = player_name.sub " ", "_"
+  player_name_symbol = :"#{player_name_with_underscores}"
+  binding.pry
+  game_hash.each do |home_away, home_away_values|
+    #binding.pry
+    home_away_values.each do |high_level, info|
+    #binding.pry
+      if high_level == :players
+        info.each do |player_names, player_stats|
+    #binding.pry
+          if player_names == player_name_symbol
+            return player_name[:points]
+            break
+          end
+        end
+      end
+    end
+  end
+end
 
-
-
-
-
-
-
-
+def shoe_size (player_name)
+  game_hash.each do |home_away, home_away_values|
+    #binding.pry
+    home_away_values.each do |high_level, info|
+    #binding.pry
+      if high_level == :players
+        info.each do |player_names, player_stats|
+    #binding.pry
+          if player_names == player_name
+            return player_name[:shoe]
+            break
+          end
+        end
+      end
+    end
+  end
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
