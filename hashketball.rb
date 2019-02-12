105d48ced86ce39ba291cff304c6749e30c06530
Adam Moran <jeffadammoran@gmail.com>
2019-02-12 06:04:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,13 +156,11 @@ def team_colors (team_name)
   game_hash.each do |home_away, home_away_values|
     home_away_values.each do |high_level, info|
       if high_level == :team_name
+        if check_val == true
+          return info
         if info == team_name 
           check_val = true
         end
-      end
-      if check_val == true
-        return "test"
-        break
       end
     end
   end
